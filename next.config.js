614f4e9af8cc7e34507804c532908e318cf38666
Wanderson Kenedy <wandersonkenedy13@gmail.com>
2023-11-20 20:39:49 -0300

    ajustando

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => {
-    // This could be anything, using the latest git hash
-    return process.env.GIT_HASH;
-  },
 };
 
 module.exports = nextConfig;
